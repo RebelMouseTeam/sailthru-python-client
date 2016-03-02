6e2a3b6eab382365cd5aa6df8e675759e0017511
Nikita Grachev <nikita.grachev@rebelmouse.com>
2016-03-02 18:07:55 +0600

    Changed requests lib from version 2.6 to 2.4 because RebelMouse uses patched requests 2.4 version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name='sailthru-client', 
-    version='2.2.2',
+setup(name='sailthru-client',
+    version='2.2.2.1',
     packages=find_packages(),
     description='Python client for Sailthru API',
     long_description=open('README.md').read(),
@@ -14,7 +14,7 @@ setup(name='sailthru-client',
         "Natural Language :: English",
     ],
     install_requires=[
-        'requests >= 2.6.0',
+        'requests == v2.4.3.4',
         'simplejson >= 3.0.7'
     ],
     keywords='sailthru api',
